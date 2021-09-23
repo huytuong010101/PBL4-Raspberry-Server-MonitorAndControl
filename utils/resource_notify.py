c53f10bc6ed16d69d925644452d39ecb5ad411ee
tuongnh <huytuong010101@gmail.com>
2021-09-23 20:29:01 +0700

    round 2 to percent

--- utils/resource_notify.py
+++ utils/resource_notify.py
@@ -6,9 +6,9 @@ import psutil
 async def loop_to_notify_resource(send_message=None, tracking=None):
     while True:
         data = {
-            "cpu": int(psutil.cpu_percent()),
-            "ram": int(psutil.virtual_memory().percent),
-            "disk": int(psutil.disk_usage(psutil.disk_partitions()[0].mountpoint).percent),
+            "cpu": round(psutil.cpu_percent(), 2),
+            "ram": round(psutil.virtual_memory().percent, 2),
+            "disk": round(psutil.disk_usage(psutil.disk_partitions()[0].mountpoint).percent, 2),
             "network": int(psutil.net_io_counters().bytes_sent/1000)
         }
         if send_message is not None:
